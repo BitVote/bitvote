b75c555d30e672719a5d9042529b3b6514dbf61b
Jasper den Ouden <o.jasper@gmail.com>
2015-03-17 23:03:51 +0100

    Test voting. Minor changes(mostly not needed)

--- test/teller.py
+++ test/teller.py
@@ -13,9 +13,10 @@ HALFWAY = 340282366920938463463374607431768211456
 s, c = None, None  # State and contract
 specials = [None, None, None]
 ids = None  # IDs that should be in there.
+zeroids = None  # IDs expected to be gone.
 
 def reset():
-    global c, s, ids
+    global c, s, ids, zeroids
     global specials, entries
     print("creating")
     if s is None:
@@ -24,7 +25,7 @@ def reset():
     c = s.abi_contract('../contracts/bitvote_teller.se', t.k0)
     print('cga', s.block.gas_used - before_gas, s.block.gas_used, before_gas)
     specials = [int(t.a0, 16), 0, 3600*24*31]
-    ids = []
+    ids, zeroids = [], []
     check()
 
 def check():
@@ -33,7 +34,10 @@ def check():
     assert c.start_vote_time() == specials[2]
     for _,a, acc in ids:
         if acc:
-            assert c.account(a) == acc, (a, c.account(a), acc)
+            assert c.account(a) == acc, (a, c.account(a) / HALFWAY, acc/HALFWAY,
+                                         c.account(a) % HALFWAY, acc%HALFWAY)
+    for a in zeroids:
+        assert c.account(a) == 0
 
 def scenario_init():
     global specials
@@ -51,8 +55,15 @@ def acc(time=None, position=None, frozen=False):
 
 def add():
     p,a = insecure_keypair()
-    c.oneperid_add(int(a, 16), sender=t.k2)
+    c.oneperid_add(a, sender=t.k2)
     ids.append((p,int(a, 16), acc()))
+
+def scenario_add():
+    scenario_init()
+    for _ in range(randrange(5,10)):
+        if random() < 0.3: s.mine()
+        add()
+    check()
 
 def move(i=None):
     i = i or randrange(len(ids))
@@ -60,37 +71,36 @@ def move(i=None):
     
     p,a = insecure_keypair()
     c.oneperid_move(aold, a, sender=t.k2)
-    ids[i] = (p, a, acc)  # Update the test memory.
+    ids[i] = (p, int(a, 16), acc)  # Update the test memory.
+    zeroids.append(aold)
 
 def freeze(i=None):
     i = i or randrange(len(ids))
     p, a, acc = ids[i]
 
     c.oneperid_freeze(a, sender=t.k2)
+    assert 2*(acc // 2) == acc - acc%2
+    assert 2*(acc // 2) == (acc if acc%2 == 0  else acc - 1)
     ids[i] = (p, a, 2*(acc // 2))
 
 def add_existing(i=None):
     i = i or randrange(len(ids))
     p, a, acc = ids[i]
 
-    c.oneperid_freeze(a, sender=t.k2)
+    c.oneperid_add(a, sender=t.k2)
     ids[i] = (p, a, 2*(acc // 2) + 1)
 
-def scenario_add():
-    scenario_init()
-    for _ in range(randrange(1,5)):
-        if random() < 0.3: s.mine()
-        add()
+# TODO remove too.
 
 def move_around():
-    for _ in range(randrange(2,5)):
+    for _ in range(randrange(5,10)):
         if random() < 0.1:
             s.mine()
         r = random()
         if r < 0.3:
             move()
-        elif r < 0.6:
-            freeze()
+##        elif r < 0.6:
+ ##           freeze()
         elif r < 0.8:
             add_existing()
         else:
@@ -101,5 +111,28 @@ def scenario_move_around():
     scenario_add()
     move_around()
 
-scenario_add()
+def vote(i=None, on_i=None, amount=None):
+    i = i or randrange(len(ids))
+    on_i = on_i or randrange(2**30)
+    amount = amount or randrange(60)
+    
+    k,a,acc =  ids[i]
+    astr = hex(a)[2:-1]
+    while len(astr) < 40:  # Damn stuff.
+        astr = "0" + astr
+    s.send(t.k0, astr, 10**14)
+    c.vote(on_i, amount, sender = k)
+
+    movtime = acc // HALFWAY + amount
+    # Something only happens if vote time available, exists, and not frozen.
+    if movtime < s.block.timestamp and acc%2 == 1:
+        ids[i] = (k,a, acc%HALFWAY + HALFWAY*movtime)
+
+def scenario_vote():
+    scenario_move_around()
+    for _ in range(20,40):
+        vote()
+    check()
+
+scenario_vote()
 
